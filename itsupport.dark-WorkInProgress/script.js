6fa8757e7550b7dcb71b162be2913463015b0b41
KriszDj2001 <57273841+KriszDj2001@users.noreply.github.com>
2023-09-28 14:07:16 +0200

    Add files via upload

--- itsupport.dark-WorkInProgress/script.js
+++ itsupport.dark-WorkInProgress/script.js
@@ -10,3 +10,20 @@
 //button.addEventListener ("click", function() {
 //  alert("Dark On");
 //});
+
+
+if (window.location.href.indexOf("ticket=") != -1) {
+	var css = '#tabs-1 > table >tbody > tr > td {background-color: transparent!important;}',
+	    head = document.head || document.getElementsByTagName('head')[0],
+	    style = document.createElement('style');
+	
+	head.appendChild(style);
+	
+	style.type = 'text/css';
+	if (style.styleSheet){
+	  // This is required for IE8 and below.
+	  style.styleSheet.cssText = css;
+	} else {
+	  style.appendChild(document.createTextNode(css));
+	}
+}
